ac3b6d1adb802db6e28888bb4f8addc4f2c01796
marcelotenaglia <tenaco23@gmail.com>
2025-04-04 15:13:32 -0300

    Creation of Social section

--- portfolio/src/AppRouter.tsx
+++ portfolio/src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import { Home } from "./components/Home/Home"
 
+
 export const AppRouter = () => {
     return (
         <>  
--- portfolio/src/components/Presentation/Presentation.tsx
+++ portfolio/src/components/Presentation/Presentation.tsx
@@ -1,4 +1,7 @@
 import './Presentation.css'
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 export const Presentation = () => {
     return (
@@ -6,6 +9,23 @@ export const Presentation = () => {
             <h1>Buenas, soy Marcelo</h1>
             <p className='studies'>Desarrolador Web, Analista de Datos</p>
             <p>Soy de San Cayetano, Buenos Aires, Argentina </p>
+            <div className='social-container'>
+                <p className='social'>@Social</p>
+                <div className='button-container'>
+                    <Link to='https://github.com/marcelotenaglia'>
+                       <button className='button'>
+                            <VscGithubAlt className='social-logo'/>
+                            <p className='p-button'>Github</p>
+                        </button>
+                     </Link>
+                     <Link to='https://www.linkedin.com/in/marcelo-tenaglia-0a473b294/'>
+                        <button className='button'>
+                            <AiOutlineLinkedin className='social-logo'/>
+                            <p className='p-button'>LinkedIn</p>
+                        </button>
+                    </Link>
+                </div>
+            </div>   
         </div>
     )
 }
